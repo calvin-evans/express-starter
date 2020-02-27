00a58182b85c9ec31a520bf92fda477e8c6ed6c0
ceuk <ceuk.dev@gmail.com>
2020-02-27 13:50:57 +0000

    remove joigoose

--- src/models/User.js
+++ src/models/User.js
@@ -1,24 +1,18 @@
 import bcrypt from 'bcrypt'
 import logger from '../services/logger'
 import Mongoose, { Schema, model } from 'mongoose'
-import Joi from 'joi'
-import Joigoose from 'joigoose'
-
-const joigoose = Joigoose(Mongoose)
 
 const error = logger('models:user', 'error')
 
-const userSchema = Joi.object({
-  firstName: Joi.string(),
-  lastName: Joi.string(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  role: Joi.string().valid(['interpreter', 'clinician', 'admin'])
-})
+const schema = new Mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  role: String,
+}, { timestamps: true})
 
-const mongooseUserSchema = new Mongoose.Schema(joigoose.convert(userSchema))
-
-mongooseUserSchema.pre('save', async function() {
+schema.pre('save', async function() {
   const user = this
   if (!user || !user.isModified('password')) return
 
@@ -30,4 +24,5 @@ mongooseUserSchema.pre('save', async function() {
   }
 })
 
-export default model('User', mongooseUserSchema)
+export default model('User', schema)
+
