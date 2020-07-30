0147fa4cbd54cb883702fcb1bddfbe70e11747a4
CEUK <1681236+ceuk@users.noreply.github.com>
2020-07-30 11:39:24 +0100

    force JWT_SECRET in production

--- src/loadenv.js
+++ src/loadenv.js
@@ -2,6 +2,10 @@ import logger from './services/logger'
 import env from 'node-env-file'
 const info = logger('config', 'info')
 
+if (process.env.NODE_ENV === 'production' && !process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET must be set in production')
+}
+
 try {
   env(`${__dirname}/../.env`)
 } catch (err) {
