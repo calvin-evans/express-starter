bf568931adfe69816593b737ea40d4b82d9045b3
ceuk <ceuk.dev@gmail.com>
2020-10-21 15:24:26 +0100

    add some tests

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -7,12 +7,70 @@ beforeEach(async () => {
   await init()
 })
 
-describe('/auth', () => {
-  test('User can log in', async (done) => {
-    await request(app)
+let token
+
+const grabToken = (done) => {
+  request(app)
       .post('/auth/login')
       .send({ email: 'ceuk.dev@gmail.com', password: 'abcd1234' })
-      .set('Accept', 'application/json')
+      .end((err, res) => {
+        if (err) return done(err)
+        token = res.body.token
+        return done()
+      })
+}
+
+const checkAuthWorks = (url, done) => {
+  request(app)
+      .get(url)
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done(err)
+      })
+}
+
+describe('/auth', () => {
+
+  test('POST /auth/login', (done) => {
+    request(app)
+      .post('/auth/login')
+      .send({ email: 'ceuk.dev@gmail.com', password: 'abcd1234' })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body).toHaveProperty('id')
+        expect(res.body).toHaveProperty('token')
+        return done()
+      })
+  })
+})
+
+describe('/user', () => {
+  beforeEach(grabToken)
+  test('GET /user/me', (done) => {
+    checkAuthWorks('/user/me', done)
+    request(app)
+      .get('/user/me')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body).toHaveProperty('id')
+        expect(res.body).toHaveProperty('firstName')
+        expect(res.body).toHaveProperty('lastName')
+        expect(res.body).toHaveProperty('email')
+        expect(res.body).toHaveProperty('createdAt')
+        expect(res.body).toHaveProperty('updatedAt')
+        return done()
+      })
+  })
+
+  test('GET /user/validateToken', async (done) => {
+    checkAuthWorks('/user/validateToken', done)
+    await request(app)
+      .get('/user/validateToken')
+      .set('Authorization', 'Bearer ' + token)
       .expect(200)
     done()
   })
